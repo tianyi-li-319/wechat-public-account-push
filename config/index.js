684944c026ea9a203f0884d0a33898ca55c12d9d
tianyi-li-319 <56812777+tianyi-li-319@users.noreply.github.com>
2022-08-29 18:29:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     // 模板消息id
     TEMPLATE_ID: "Uidcr4qEr-1_b2IKzhkhBHar0rT_0I_3tBHFcUi3-Uo",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: ["o_BJR6bGOXwQTBq34bWaSA9yy_PQ",:"o_BJR6YMe6kWwDE69VGPldsg4ozE"],
+    USERS: ["o_BJR6bGOXwQTBq34bWaSA9yy_PQ","o_BJR6YMe6kWwDE69VGPldsg4ozE"],
      
     /**
      * 信息配置
